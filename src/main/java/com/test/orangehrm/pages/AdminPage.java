0ddc95086191b58931b75034142e5151617af00a
techtorial <info@techtorialacademy.com>
2022-06-04 13:09:09 -0500

    Last Version of Selenium mentoring

--- src/main/java/com/test/orangehrm/pages/AdminPage.java
+++ src/main/java/com/test/orangehrm/pages/AdminPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utils.BrowserUtils;
+
+import java.util.List;
 
 public class AdminPage {
 
@@ -14,8 +17,59 @@ public class AdminPage {
     @FindBy(id = "btnAdd")
     WebElement addButton;
 
+    @FindBy(id = "systemUser_userType")
+    WebElement userRole;
+
+    @FindBy(id = "systemUser_employeeName_empName")
+    WebElement employeeName;
+
+    @FindBy(id = "systemUser_userName")
+    WebElement userName;
+
+    @FindBy(name = "systemUser[status]")
+    WebElement status;
+
+    @FindBy(xpath = "//input[@name='systemUser[password]']")
+    WebElement password;
+
+    @FindBy(id = "systemUser_confirmPassword")
+    WebElement confirmPassword;
+
+    @FindBy(id = "btnSave")
+    WebElement saveButton;
+
+    @FindBy(xpath = "//a[contains(@href,'saveSystemUser')]")
+    List<WebElement> allNames;
+
     public void clickAddButton(){
         addButton.click();
     }
+
+    public void sendEmployeeInformation(String role,String name,String userName,String statusName
+    ,String password,String confirmPassword) throws InterruptedException {
+        BrowserUtils.selectBy(userRole,role,"text");
+        employeeName.sendKeys(name);
+        this.userName.sendKeys(userName);
+        BrowserUtils.selectBy(status,statusName,"text");
+        this.password.sendKeys(password);
+        this.confirmPassword.sendKeys(confirmPassword);
+        Thread.sleep(1000);
+        saveButton.click();
+        Thread.sleep(1000);
+    }
+
+    public boolean validationUserNameIsCreated(String userName){
+
+        for(WebElement name:allNames){
+            if(name.getText().trim().equals(userName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
 
 }
--- src/main/java/com/test/orangehrm/tests/AdminTest.java
+++ src/main/java/com/test/orangehrm/tests/AdminTest.java
@@ -6,6 +6,7 @@ import com.test.orangehrm.pages.MainPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,13 +21,7 @@ public class AdminTest {
     }
     @Test
     public void validateUrlAndAddButton() throws InterruptedException {
-        LoginPage loginPage=new LoginPage(driver);
-        loginPage.sendInformation("Admin","admin123");
-        MainPage mainPage=new MainPage(driver);
-        mainPage.clickAdmin();
-        AdminPage adminPage=new AdminPage(driver);
-        adminPage.clickAddButton();
-        /*
+         /*
         1-Choose the User Role "Admin"
         2-Employee Name "Alice Duval"
         3-Username can be anything but should be different for each run
@@ -35,6 +30,16 @@ public class AdminTest {
         6-put your confirm password
         7-Click save button
          */
+        LoginPage loginPage=new LoginPage(driver);
+        loginPage.sendInformation("Admin","admin123");
+        MainPage mainPage=new MainPage(driver);
+        mainPage.clickAdmin();
+        AdminPage adminPage=new AdminPage(driver);
+        adminPage.clickAddButton();
+        adminPage.sendEmployeeInformation("Admin","Alice Duval","ahmet36",
+                "Disabled","12345678","12345678");
+        Assert.assertTrue(adminPage.validationUserNameIsCreated("ahmet36"));
+
 
 
 
